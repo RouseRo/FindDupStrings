0229515f07fe0d3bfe84ce0e665a0caf93323709
RouseRo <robert_k_rouse99@yahoo.com>
2020-09-27 12:04:18 -0700

    Added the  GetFirstOccuranceOfStr(), and test data: One Dup Str 3

--- FindDuplicateStrings.py
+++ FindDuplicateStrings.py
@@ -5,13 +5,14 @@
 # Title: The Find Duplicate Strings Project
 # Author: Robert Rouse
 # Created Date: 09-11-2020, 12:15
-# Last Update:  09-14-2020, 16:32
+# Last Update:  09-15-2020, 16:24
 #
 # History:
 #   Added the DuplicateStringsResults class
 #   Added the DuplicateStringTestData class and the FindDupStrings_MethodPassedData()
 #   Added Calling FDS_Methods by function number
 #   Adding the Hash method
+#   Added GetFirstOccuranceOfStr()
 #
 #==================================================================================================
 #==================================================================================================
@@ -47,6 +48,13 @@ class DuplicateStringTestData:
         self.testDataList = testDataList
         self.solutionLists = solutionLists
 
+def GetFirstOccuranceOfStr(currentPos, testDataList):
+    targetStr = testDataList[currentPos]
+    for i in range(currentPos - 1, -1, -1):
+        if testDataList[i] == targetStr:
+            return i
+    return -1
+
 def FindDupStrings_Random_Method():
     dtStart = datetime.datetime.now()
 
@@ -75,19 +83,20 @@ def FindDupStrings_Hash_Method(DuplicateStringTestData):
 
     pos = -1
     for str2hash in DuplicateStringTestData.testDataList:
-        solutionLists.append(pos)
         pos = pos + 1
         hashIndex = abs(hash(str2hash)) % hashTableSize
         print(f"str[{str2hash}] hash index:[{hashIndex}]")
         hashTable[hashIndex] = hashTable[hashIndex] + 1
-        if hashTable[hashIndex] > 1:
+        if hashTable[hashIndex] == 2:
             print(f" {str2hash} {hashTable[hashIndex]} is a duplicate")
-
-            # Is this a string the same as the previous 
-
+            # Find the first occurance of the dup string
+            firstPos = GetFirstOccuranceOfStr(pos,DuplicateStringTestData.testDataList)
+            solutionLists.append(firstPos)
             solutionLists.append(pos)
-        else:
-            solutionLists.pop()
+
+        if hashTable[hashIndex] > 2:
+            print(f" {str2hash} {hashTable[hashIndex]} is another duplicate")
+            solutionLists.append(pos)
 
     dtEnd = datetime.datetime.now()
     method_elapsed_time = dtEnd - dtStart 
@@ -132,6 +141,7 @@ async def main():
     dupStringTestData.append(DuplicateStringTestData("No Dups", ["abcd1", "abcd2", "ABCD3"],[]))
     dupStringTestData.append(DuplicateStringTestData("One Dup Str 1", ["abcd1", "abcd2", "ABCD3", "DUP01", "DUP01","ABCD5"],[3,4]))
     dupStringTestData.append(DuplicateStringTestData("One Dup Str 2", ["abcd1", "DUP01", "abcd2", "ABCD3", "DUP01","ABCD5"],[1,4]))
+    dupStringTestData.append(DuplicateStringTestData("One Dup Str 3", ["DUP01", "abcd2", "abcd3", "ABCD4", "ABCD5","DUP01"],[0,5]))
     dupStringTestData.append(DuplicateStringTestData("Multiple Dup Strs", ["abcd1", "abcd2", "ABCD3", "DUP01", "DUP01","ABCD5", "DUP02", "ABCD7","DUP02"],
                                                      [[3, 4], [6,8]]))
 
