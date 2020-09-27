c14cdc5dc1cc0f721f32138469ccfbf9965db0bf
RouseRo <robert_k_rouse99@yahoo.com>
2020-09-26 18:24:12 -0700

    Simplified Main(), Added class DuplicateStringTestData:

--- FindDuplicateStrings.py
+++ FindDuplicateStrings.py
@@ -22,7 +22,7 @@ strData = ["abcd1", "abcd2", "ABCD3", "DUP01", "DUP01","ABCD5", "DUP02", "ABCD7"
 solutionData  = [[3, 4], [6,8]]
 solutionData2 = [[3, 5], [6,8]]
 
-methodResults = []
+dupStringTestData = []
 
 class DuplicateStringsResults:
     def __init__(self, elapsed_time, indexList):
@@ -36,6 +36,12 @@ class DuplicateStringsResults:
             return False
 
 
+class DuplicateStringTestData:
+    def __init__(self, name, testDataList, solutionList):
+        self.name = name
+        self.testDataList = testDataList
+        self.solutionList = solutionList
+
 def FindDupStrings_Method(methNum):
     dtStart = datetime.datetime.now()
 
@@ -46,37 +52,34 @@ def FindDupStrings_Method(methNum):
 
     dtEnd = datetime.datetime.now()
     method_elapsed_time = dtEnd - dtStart 
+    return DuplicateStringsResults(method_elapsed_time,solutionData)
 
-    # Insert the elapsed time and solution-list
-    methodResults.insert(methNum - 1, DuplicateStringsResults(method_elapsed_time,solutionData))
-  
+def FindDupStrings_MethodPassedData(methNum, DuplicateStringTestData):
+
+    methodResults = FindDupStrings_Method(methNum)
+    if methodResults.indexList == DuplicateStringTestData.solutionList:
+        resultStr = "PASS"
+    else:
+        resultStr = "FAIL"
+    print(f" {resultStr}: Medthod: [{methNum}], Test Data Name: **{DuplicateStringTestData.name}**, Elapsed Time: {methodResults.elapsed_time}")
     return
-
 
 async def main():
     print("Main start")
+
+    # Build test data
+    dupStringTestData.append(DuplicateStringTestData("No Dups", ["abcd1", "abcd2", "ABCD3"],[]))
+    dupStringTestData.append(DuplicateStringTestData("One Dup Str", ["abcd1", "abcd2", "ABCD3", "DUP01", "DUP01","ABCD5"],[3,4]))
+    dupStringTestData.append(DuplicateStringTestData("Multiple Dup Strs", ["abcd1", "abcd2", "ABCD3", "DUP01", "DUP01","ABCD5", "DUP02", "ABCD7","DUP02"],
+                                                     [[3, 4], [6,8]]))
+
     print()
 
-    expectedResults = DuplicateStringsResults(0, solutionData)
-
-    FindDupStrings_Method(1)
-    FindDupStrings_Method(2)
-
-    print()
-    i = 0
-    for methResult in methodResults:
-        i = i + 1
-        print(f"*** Elapsed Time for method {i} is {methResult.elapsed_time} in seconds ***")
-
-        if methResult.IsValidResult(solutionData):
-            print(f"*** Method {i} is Valid ***")
-        else:
-            print(f"*** Method {i} is Not Valid ***")
-
-        if methResult.IsValidResult(solutionData2):
-            print(f"*** Method {i} is Valid ***")
-        else:
-            print(f"*** Method {i} is Not Valid ***")
+    # Run all the test data thru all the methods.
+    for i in range(1,3):
+        for testData in dupStringTestData:
+                FindDupStrings_MethodPassedData(i, testData)
+                print()
 
     print()
 
